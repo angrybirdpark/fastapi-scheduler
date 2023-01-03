e721e963ee967d729a269e8c62af362a9ab125a0
unknown <thesmileless90@gmail.com>
2023-01-03 17:42:59 +0900

    day1 practice

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
+from datetime import datetime, timedelta
+
 from fastapi import FastAPI
+from fastapi import requests
+
+from pydantic import BaseModel
+
+from typing import Optional
 
 app = FastAPI()
 
+class User(BaseModel):
+    id : int
+
+class Schedule(BaseModel):
+    id : int
+    start_time : int
+    interval_time : int
+    user : User
+
 @app.get("/")
-def read_root() :
+async def read_root() :
     return {"message" : "success"}
 
-@app.get("/{user_id}/{start_time}/{interval_time}")
-def read_item(user_id : int, start_time : int, interval_time = int) :
-    return {"user_id" : user_id, "start_time" : start_time, "interval_time" : interval_time}    
+# @app.post("/schedules/", response_model=Schedule)
+# async def create_item(schedule: Schedule):
+#     data = requests.post(data)
+#     start_time = data[start_time]
+#     return start_time
+
+@app.get("/{user_id}/{start}/{interval}/")
+async def read_schedule(user_id: int, start: int, interval: int):
+    today = datetime.now().date()
+    start_time = datetime(today.year, today.month, today.day, hour=start)
+    cnt = 24 // interval
+    result = []
+    for i in range(cnt):
+        added_time = start_time + timedelta(hours=interval*i)
+        result.append(added_time.strftime("%Y-%m-%d-%H-%M-%S"))
+    return result
