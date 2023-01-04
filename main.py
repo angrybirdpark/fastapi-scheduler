bcb9a605d8c410c49f3382af63acc6ece2c2f662
unknown <thesmileless90@gmail.com>
2023-01-04 15:58:52 +0900

    Add : scheduler ver.2

--- main.py
+++ main.py
@@ -8,28 +8,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+schedule = BackgroundScheduler(timezone='Asia/Seoul')
+schedule.start()
+
 @app.get("/")
 async def read_root() :
     return {"message" : "root"}
 
 @app.get("/schedule/{user_id}/{start}/{interval}/")
 async def read_schedule(user_id: int, start: int, interval: int):
-    start_time = datetime.now()+timedelta(seconds=start)
-
     def scheduler():
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"user_id : {user_id}, start_time : {start_time:%Y-%m-%d %H:%M:%S}, current_time : {current_time}")
-
-    schedule = BackgroundScheduler(timezone='Asia/Seoul')
-    schedule.start() 
-
-    schedule.add_job(
-        scheduler,
-        'interval',
-        seconds    = interval,
-        start_date = start_time,
-        id         = "scheduler" + str(user_id)
-        )
+    
+    start_time = datetime.now()+timedelta(seconds=start)
+    schedule_id = str(user_id)
+    
+    if schedule.get_job(schedule_id):
+        schedule.remove_job(schedule_id)
+    
+    schedule.add_job(scheduler, 'interval', seconds=interval, start_date=start_time, id=schedule_id)
 
     result = dict()
     result.update(
