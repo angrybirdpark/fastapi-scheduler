9a38b664b28cf3e127b74bfe126ae8f22660b330
unknown <thesmileless90@gmail.com>
2023-01-04 09:11:42 +0900

    Add : ready to post

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from datetime import datetime, timedelta
 
 from fastapi import FastAPI
@@ -28,13 +29,17 @@ async def read_root() :
 #     start_time = data[start_time]
 #     return start_time
 
-@app.get("/{user_id}/{start}/{interval}/")
+@app.get("/schedule/{user_id}/{start}/{interval}/")
 async def read_schedule(user_id: int, start: int, interval: int):
     today = datetime.now().date()
     start_time = datetime(today.year, today.month, today.day, hour=start)
-    cnt = 24 // interval
-    result = []
+    cnt = math.ceil(24 / interval)
+    result = dict()
+    result_time = list()
+    
     for i in range(cnt):
         added_time = start_time + timedelta(hours=interval*i)
-        result.append(added_time.strftime("%Y-%m-%d-%H-%M-%S"))
+        result_time.append(added_time.strftime("%Y-%m-%d-%H-%M-%S"))
+    result['user_id'] = user_id
+    result['schedule'] = result_time
     return result
