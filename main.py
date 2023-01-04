4d4f4871e06c2aaf2c1e81a395debb3f3fa79a0f
unknown <thesmileless90@gmail.com>
2023-01-04 13:53:09 +0900

    Add : scheduler ver.1

--- main.py
+++ main.py
@@ -1,76 +1,75 @@
-import math, uvicorn, time
+import uvicorn
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from datetime import datetime, timedelta
 
 from fastapi import FastAPI
-from fastapi import requests
 
 from pydantic import BaseModel
 
 from typing import Optional
 
-from scheduler import job
-
-from apscheduler.schedulers.background import BackgroundScheduler
-
 app = FastAPI()
-
-class User(BaseModel):
-    id : int
-
-class Schedule(BaseModel):
-    id : int
-    start_time : int
-    interval_time : int
-    user : User
 
 @app.get("/")
 async def read_root() :
-    return {"message" : "success"}
-
-# @app.post("/schedules/", response_model=Schedule)
-# async def create_item(schedule: Schedule):
-#     data = requests.post(data)
-#     start_time = data[start_time]
-#     return start_time
+    return {"message" : "root"}
 
 @app.get("/schedule/{user_id}/{start}/{interval}/")
-async def read_schedule(user_id: int, start: int, interval: float):
-    today = datetime.now().date()
-    start_time = datetime(today.year, today.month, today.day, hour=start)
-    cnt = math.ceil(24/interval)
-    result = dict()
-    result_time = list()
+async def read_schedule(user_id: int, start: int, interval: int):
+    start_time = datetime.now()+timedelta(seconds=start)
 
-    def job2():
-        print(f"user_id : {user_id}, time : {datetime.now()}")
+    def scheduler():
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"user_id : {user_id}, start_time : {start_time:%Y-%m-%d %H:%M:%S}, current_time : {current_time}")
 
+    schedule = BackgroundScheduler(timezone='Asia/Seoul')
+    schedule.start() 
 
-
-
-
-    sched = BackgroundScheduler(timezone='Asia/Seoul') #백그라운드로 실행하기 위해 선언
-    sched.start() 
-
-    sched.add_job(
-        job2,
+    schedule.add_job(
+        scheduler,
         'interval',
-        seconds=1,
-        id="scheduler") # 수행할 함수를 job에 등록
-    #1초 마다 한번씩 함수를 수행한다.
+        seconds    = interval,
+        start_date = start_time,
+        id         = "scheduler" + str(user_id)
+        )
 
-
-
-
-
-
-
-    for i in range(cnt):
-        added_time = start_time + timedelta(hours=interval*i)
-        result_time.append(added_time.strftime("%Y-%m-%d-%H-%M-%S"))
-    result['user_id'] = 3
-    result['schedule'] = result_time
+    result = dict()
+    result.update(
+        user_id  = user_id,
+        now      = datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        start    = start_time.strftime("%Y-%m-%d %H:%M:%S"),
+        interval = interval
+        )
     return result
 
+# @app.get("/schedule/{user_id}/{start}/{interval}/")
+# async def read_schedule(user_id: int, start: int, interval: int):
+#     start_time = datetime.now() + timedelta(seconds=start)
+    
+#     def scheduler():
+#         print(f"user_id : {user_id}, time : {datetime.now()}")
+
+#     sched = BackgroundScheduler(timezone='Asia/Seoul')
+#     sched.start() 
+
+#     sched.add_job(
+#         scheduler,
+#         'interval',
+#         seconds=interval,
+#         start_date = start_time,
+#         id="scheduler"
+#         )
+
+#     result = dict()
+#     result.update(
+#         user_id  = user_id,
+#         now      = datetime.now(),
+#         start    = start_time,
+#         interval = interval
+#         )
+#     return result
 
 if __name__ == '__main__':
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
